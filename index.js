4727043faf29d38057e075c7741151aed2518ff0
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-05-30 17:54:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express();
 app.use(cors())
 require("dotenv").config();
 const port = process.env.PORT || 8000
-const {readdirSync} = require("fs")
-
-
-readdirSync("./routes").map((file)=>app.use("/",require("./routes/"+file)))
-
 
 app.get("/", (req, res) => {
   res.send("Main page!");
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);

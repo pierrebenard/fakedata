e89cf48dbccdc47d23c654095d912a0639522d5c
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-05-30 18:03:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require("cors")
 const app = express();
 app.use(cors());

7e5afc3e19dc2e13013b6ab63ce2f6297a3910ae
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-05-30 17:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors")
 const app = express();
-app.use(cors())
-require("dotenv").config();
-const port = process.env.PORT || 8000
+app.use(cors());
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("Main page!");
